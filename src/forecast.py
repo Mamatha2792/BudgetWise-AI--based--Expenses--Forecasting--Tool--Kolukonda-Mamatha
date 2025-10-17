42d54ea9764f96d70e10fdd6a51bee95aaa50c5d
Mamatha2792 <vy863170@gmail.com>
2025-10-17 15:07:22 +0530

    Add predict_future function for future predictions

--- src/forecast.py
+++ src/forecast.py
@@ -1,1 +1,10 @@
+# src/forecast.py
+import numpy as np
+
+def predict_future(model, df, periods=3):
+    last = int(df['month_index'].max())
+    future_idx = np.array([[i] for i in range(last+1, last+1+periods)])
+    preds = model.predict(future_idx)
+    # return list of floats
+    return [float(round(x, 2)) for x in preds]
 
